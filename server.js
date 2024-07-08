dcb2ecbb22aa5c66d481ad1523c5c1042f535b28
melissaceee <cheng.melissa@outlook.com>
2024-07-08 16:55:58 -0400

    rolling the dice

--- server.js
+++ server.js
@@ -1,15 +1,31 @@
-//BE POLITE, GREET THE USER
 
 const express = require('express')
 const app = express();
 
-app.get('/greetings/:username', (req, res) => {
-    res.send(`Hello there, ${req.params.username}!`);
-});
+//BE POLITE, GREET THE USER
+
+// app.get('/greetings/:username', (req, res) => {
+//     res.send(`Hello there, ${req.params.username}!`);
+// });
+
+//ROLLING THE DICE
+
+// app.get ('/roll/:number', (req, res)=> {
+//     const number = parseInt (req.params.number,10);
+//     if (isNaN(number)) {
+//         res.send ('You must specify a number.')
+//     } else{
+//         const randomRoll = Math.floor(Math.random()*(number+1))
+//         res.send (`You rolled ${randomRoll}.`)
+//     }
+// })
+
+// I want THAT ONE
+
+
 
 app.listen(3000,()=> {
     console.log (`server is running`)
 })
 
 
-//ROLLING THE DICE
